7b84c0050165c2c90dc31e45a84d3b7a52fe0971
Mellef <mellef_med_ali@hotmail.fr>
2022-10-30 22:33:46 +0100

    final version for checkpoint

--- src/components/list.js
+++ src/components/list.js
@@ -10,50 +10,56 @@ export default function List() {
     const taskList = useSelector((state) => state.taskList.value)
     const [initialList, setInitialList] = useState(taskList)
     const [filtered, setFiltered] = useState(false)
-    const [filterStatus, setFiltreStatus] = useState("all")
+    const [isVisible, setIsVisible] = useState("visible")
+    // const [filterStatus, setFiltreStatus] = useState("all")
 
     const dispatch = useDispatch()
 
-    function toggleFilterStatus() { filterStatus=="done"?setFiltered("notyet"):setFiltered("done") }
+    // function toggleFilterStatus() { filterStatus == "done" ? setFiltered("notyet") : setFiltered("done") }
 
-    function handleFilter() {
+    function handleFilterByNotyet() {
+        setInitialList(taskList)
+        dispatch(filterList(taskList.filter((task) => task.isDone == false)))
+        setIsVisible("hidden")
+        // toggleFilterStatus()
+    }
+    function handleFilterByDone() {
         // setInitialList(taskList)
-        // dispatch(filterList(initialList.filter((task) => task.isDone == filtered)))
-        toggleFilterStatus()
+        dispatch(filterList(taskList.filter((task) => task.isDone == true)))
+        // toggleFilterStatus()
+        setIsVisible("hidden")
     }
 
 
     function handleNotFiltered() {
-        filterStatus("all")
-        dispatch(filterList(taskList))
+        // filterStatus("all")
+        setInitialList(taskList)
+        dispatch(filterList(initialList))
         setFiltered(false)
+        setIsVisible("visible")
+
         console.log("logged")
     }
 
 
     return (
         <div>
-            <AddTask />
-            <button onClick={handleFilter}>
-                Filter {filtered ? "Done" : "NotYet"}
+            <button onClick={
+                () => {
+                    setInitialList(taskList);
+                    handleFilterByDone();
+                }
+            } style={{ visibility: isVisible }}>
+                Filter by Done
             </button>
-            <button onClick={handleNotFiltered}>
-                X
+            <button onClick={handleFilterByNotyet} style={{ visibility: isVisible }}>
+                Filter by Not Yet
+            </button>
+            <button onClick={handleNotFiltered} style={{ visibility: (isVisible=="visible"?"hidden":"visible") }}>
+                Effacer le filter !
             </button>
             <div className='taskList'>
-                {initialList.filter(
-                    
-                    ()=>{
-                        switch (filterStatus) {
-                            case "done":(e)=>e.isDone==true; break;
-                            case "notyet":e=>e.isDone==false; break;
-                            case "all":e=>e; break;
-                            default:
-                                break;
-                        }
-                    }
-                
-                ).map(
+                {taskList.map(
                     (e) => <Task key={e.id} list={e} />
                 )}
             </div>
